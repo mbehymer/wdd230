af42b68af003dc724845111b319abdaa532f2959
Matthew Behymer <beh20004@byui.edu>
2022-11-21 10:01:10 -0700

    typescript additions

--- typscript/hello-world/index.js
+++ typscript/hello-world/index.js
@@ -1,3 +1,20 @@
+//primitive types number, string, boolean
 var age = 30; //The colon is used to describe the type of variable
-age = "string";
-
+//age = "string"; //If you try to set age to a string, you will see that there is an error 
+//and that it will display an error in the terminal when you try to compile it.
+var a = "some string";
+// a = false;
+var b = false;
+// b = 2;
+var arr = [1, 2, 3];
+// arr.push("four");
+var otherArr = [1, "two", "three", 4, 5, 6];
+// otherArr.push(false);
+function myFunc(y) {
+    return y;
+    // return y.toString()
+    // return y === 2;
+}
+// myFunc = "something";
+var inf = 1 / 0;
+console.log(inf);
--- typscript/hello-world/index.ts
+++ typscript/hello-world/index.ts
@@ -1,6 +1,40 @@
+//primitive types number, string, boolean
+
 let age: number = 30;//The colon is used to describe the type of variable
 
 //age = "string"; //If you try to set age to a string, you will see that there is an error 
                   //and that it will display an error in the terminal when you try to compile it.
 
-    
+
+let a = "some string";
+
+// a = false;
+
+let b = false;
+
+// b = 2;
+
+
+let arr: number[] = [1,2,3];
+
+// arr.push("four");
+
+let otherArr: (string | number)[] = [1, "two", "three", 4, 5, 6]
+
+// otherArr.push(false);
+
+function myFunc(y: number): (number | string) {
+
+    return y
+
+    // return y.toString()
+
+    // return y === 2;
+}
+
+// myFunc = "something";
+
+let inf = 1/0;
+
+console.log(inf);
+
